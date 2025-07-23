910e729d5a4d9454c22f3274b77c18b4df6be6c4
Aryan Rai <aryanrai97861@gmail.com>
2025-07-23 22:47:21 +0530

    initial commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,11 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import MarketCalendar from "@/pages/market-calendar";
-import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={MarketCalendar} />
-      <Route component={NotFound} />
     </Switch>
   );
 }
